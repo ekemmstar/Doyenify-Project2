0535de79b2116a657688531b0b9db304bcf85316
ekemmstar <103825682+ekemmstar@users.noreply.github.com>
2023-11-19 04:46:45 +0200

    Your commit message here

---
+++ src/data.js
@@ -0,0 +1,92 @@
+
+export const Data = {
+    cardData:[
+        {
+            id:1,
+            img:require("../src/images/img1.jpg"),
+            title:'blog 1',
+            desc: "Nature's beauty is a harmonious blend of stunning landscapes and delicate details. Majestic mountains stand as silent sentinels, their peaks touching the sky, while tranquil lakes mirror the serenity of their surroundings."
+
+        },
+        {
+            id:2,
+            img:require("../src/images/img2.jpg"),
+            title:'blog 2',
+            desc: 'A staircase is an architectural element designed to provide a structured ascent or descent between different levels or floors within a building. It typically consists of a series of steps or treads, each of which is a horizontal surface, and risers, the vertical components that form the face of each step'
+
+        },
+        {
+            id:3,
+            img:require("../src/images/img3.jpg"),
+            title:'blog 3',
+            desc: 'A home is more than just a physical structure; it is a haven that encapsulates the essence of comfort, belonging, and personal expression. It comprises rooms and spaces tailored to meet the needs and desires of its inhabitants'
+
+        },
+        {
+            id:4,
+            img:require("../src/images/img4.jpg"),
+            title:'blog 4',
+            desc: 'A mountain or hill is a landform that rises prominently above its surroundings, characterized by elevated terrain and steep slopes. Mountains are typically larger and more imposing, often forming part of vast mountain ranges, while hills are smaller in scale. '
+
+        },
+        {
+            id:5,
+            img:require("../src/images/img5.jpg"),
+            title:'blog 5',
+            desc: "The ocean is a vast body of saltwater that covers the majority of the Earth's surface. It is a dynamic and constantly changing ecosystem that plays a crucial role in regulating the planet's climate and supporting a diverse range of life"
+
+        },
+        {
+            id:6,
+            img:require("../src/images/img6.jpg"),
+            title:'blog 6',
+            desc: "It consists of various zones, from the sunlit surface layers to the dark, mysterious depths, each hosting unique marine life adapted to its specific conditions. The ocean's currents, waves, and tides create a symphony of movement, and its waters are home to an incredible diversity of organisms, from microscopic plankton to majestic whales."
+
+        },
+        {
+            id:7,
+            img:require("../src/images/img7.jpg"),
+            title:'blog 7',
+            desc: "Architectural design is the creative and technical process of conceiving, planning, and crafting structures, spaces, and environments. It involves the synthesis of aesthetic, functional, and practical considerations to meet the needs and aspirations of users while responding to the context and purpose of the project"
+
+        },
+        {
+            id:8,
+            img:require("../src/images/img8.jpg"),
+            title:'blog 8',
+            desc: "Studying is a dynamic and purposeful process of acquiring knowledge, skills, and understanding. It involves focused engagement with educational materials, exploration of ideas, and the development of critical thinking. Through study, individuals cultivate intellectual curiosity, deepen their expertise, and prepare themselves for personal and professional growth.."
+
+        },
+        {
+            id:9,
+            img:require("../src/images/img9.jpg"),
+            title:'blog 9',
+            desc: "A room with a library is a haven for knowledge and contemplation. Walls lined with shelves house a curated collection of books, creating an atmosphere of intellectual richness. The scent of well-worn pages mingles with the quiet rustle of turning leaves as one navigates through the literary landscape"
+
+        },
+        {
+            id:10,
+            img:require("../src/images/img10.jpg"),
+            title:'blog 10',
+            desc: "Tall buildings in architecture stand as towering expressions of human innovation and engineering prowess. These structures, often skyscrapers, redefine city skylines, showcasing the melding of artistic design and functional necessity. "
+
+        },
+        {
+            id:11,
+            img:require("../src/images/img11.jpg"),
+            title:'blog 11',
+            desc: "Books are tangible portals to worlds of imagination, knowledge, and emotion. These bound collections of written or printed pages serve as vessels for storytelling, information, and the exploration of ideas. "
+
+        },
+        {
+            id:12,
+            img:require("../src/images/img12.jpg"),
+            title:'blog 12',
+            desc: "It's a moment of connection with nature, where the timeless dance of the tides and the gentle caress of the oceanic surroundings create a serene and rejuvenating ambiance."
+
+        }
+
+    ]
+
+}
+export default Data;
